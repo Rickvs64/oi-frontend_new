27ff780fa6dbb1153bec7fe8d39040fa9289386a
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2020-01-06 16:20:48 +0100

    Modify websocket url

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
 
     <div id="response">
-      <p>Text response.</P>
+      <p>Large block of text that's supposed to be multi-line.</P>
     </div>
 
   </div>
@@ -19,12 +19,19 @@ export default {
     };
   },
   mounted() {
-    
+    this.openSocket();
   },
   methods: {
     playAudio(audioFile) {
       var snd = new Audio("data:audio/wav;base64," + audioFile);
       snd.play();
+    },
+    openSocket() {
+      var socket = new WebSocket("ws://localhost:8089/socket");
+      
+      socket.onmessage = function (event) {
+        console.log(event.data);
+      }
     }
   }
 }
