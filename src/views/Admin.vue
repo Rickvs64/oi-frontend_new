5cf2873217fb7a5811c0a7359f95dc191e888dc1
Captain Capslock and 7 others <Rickvs64@users.noreply.github.com>
2020-01-06 11:33:54 +0100

    Remove obsolete components

--- src/views/Admin.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="admin">
-    <h1>Admin</h1>
-
-    <h2>Dashboard page</h2>
-    <p>This page will be used in the future to specify back-end URL and manage the bot's personality directly.</p>
-  </div>
-</template>
